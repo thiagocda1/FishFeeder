d60630d4b7c649fde7e14d19e231f7e645a763fe
Thiago Calixto de Almeida <thiago.calixto@academico.ifrn.edu.br>
2018-11-08 09:20:23 -0300

    alterando o nome das pastas e juntando o script do banco de dados

--- configuracao/script.sql
+++ configuracao/script.sql
@@ -1,6 +1,16 @@
 create database FishFeeder;
 use FishFeeder;
 
+create table animais (
+codigo int auto_increment,
+idade int (3),
+nome varchar(255),
+sexo varchar(255),
+primary key (codigo));
+
+insert into animais (nome,idade, sexo)
+values ('leão', '1', 'masculino');
+
 create table agendamento (
 codigo int auto_increment,
 hora varchar (255),
